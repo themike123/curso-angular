1ae9afcaa865c744c9ca345a5b0c452bef6d6ec2
Michel Arriaga <marriaga@elektra.com.mx>
2021-05-10 12:45:38 -0500

    Add service users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule  } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { PantallaDetalleComponent } from './pantalla-detalle/pantalla-detalle.co
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
